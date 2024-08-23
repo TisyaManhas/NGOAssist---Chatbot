a96e8bcd5cc4f0f8dccd8ed54962c2d3ecd6e805
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-08-23 12:53:39 +0530

    backend fixes an box show/hide

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import ChatBox from "./components/ChatBox";
@@ -8,8 +8,29 @@ function App() {
   const [chatBoxOpen, setChatBoxOpen] = useState(false);
 
   return (
-    <ChatBox/>
-  )
+    <div className="w-full h-screen items-center flex justify-center manrope">
+      {chatBoxOpen && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="absolute bottom-20 right-20"
+        >
+          <ChatBox />
+        </motion.div>
+      )}
+      {!chatBoxOpen && (
+        <div
+          onClick={() => {
+            setChatBoxOpen(!chatBoxOpen);
+          }}
+          className="absolute bottom-10 right-10 z-10 rounded-full bg-red-400 w-fit p-2"
+        >
+          Chatbot
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
