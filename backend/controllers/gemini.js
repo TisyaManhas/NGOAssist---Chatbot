e41ef444acf3c378943fa425bbe8641375605392
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-08-23 09:26:52 +0530

    added gemini package

---
+++ backend/controllers/gemini.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash",
+    systemInstructions: 
+ });
+
+async function run() {
+  const prompt = "Write a story about an AI and magic in 30 words";
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+async function generateContent(text) {
+  const result = await model.generateContent(text);
+  const response = await result.response;
+  const text = response.text();
+  return text;
+}
+
+module.exports = { generateContent };
