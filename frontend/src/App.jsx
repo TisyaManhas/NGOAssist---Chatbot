d344a36f530b703078126c40419081912f9e2da8
Nirmat singh <nimratsingh5340@gmail.com>
2024-08-23 09:16:04 +0530

    added a chatbox

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
+import ChatBox from './components/ChatBox'
 // import Abc from './components/abc'
 // import Header from './components/header'
 
@@ -8,12 +9,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-       <Routes>
-        {/* <Route path='/' element={<Abc/>}/>
-        <Route path='/about' element={<Header/>} /> */}
-       </Routes>
-    </BrowserRouter>
+    <ChatBox/>
   )
 }
 
