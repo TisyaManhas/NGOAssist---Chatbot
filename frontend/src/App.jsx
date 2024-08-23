c0894c2f9607a5408549f3f1ee5fc095220e6912
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-08-23 12:58:49 +0530

    backend fixes an box show/hide

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,29 +6,47 @@ import { motion } from "framer-motion";
 
 function App() {
   const [chatBoxOpen, setChatBoxOpen] = useState(false);
-
+  useEffect(() => {
+    addEventListener("click", (e) => {
+      if (e.target.className !== "absolute bottom-20 right-20") {
+        setChatBoxOpen(false);
+      }
+    });
+    return () => {
+      removeEventListener("click", (e) => {
+        if (e.target.className !== "absolute bottom-20 right-20") {
+          setChatBoxOpen(false);
+        }
+      });
+    };
+  }, []);
   return (
     <div className="w-full h-screen items-center flex justify-center manrope">
-      {chatBoxOpen && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
-          className="absolute bottom-20 right-20"
-        >
-          <ChatBox />
-        </motion.div>
-      )}
-      {!chatBoxOpen && (
-        <div
-          onClick={() => {
-            setChatBoxOpen(!chatBoxOpen);
-          }}
-          className="absolute bottom-10 right-10 z-10 rounded-full bg-red-400 w-fit p-2"
-        >
-          Chatbot
-        </div>
-      )}
+      <div onClick={(e) => e.stopPropagation()}>
+        {chatBoxOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="absolute bottom-20 right-20"
+          >
+            <ChatBox />
+          </motion.div>
+        )}
+        {!chatBoxOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            onClick={() => {
+              setChatBoxOpen(!chatBoxOpen);
+            }}
+            className="absolute bottom-10 right-10 z-10 rounded-full bg-red-400 w-fit p-2"
+          >
+            Chatbot
+          </motion.div>
+        )}
+      </div>
     </div>
   );
 }
